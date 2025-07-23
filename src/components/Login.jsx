c31acd9f88acd901eec9605bc7d09850ae32d766
Neha Kashyap <kashyapneha84@gmail.com>
2025-07-23 16:40:32 +0530

    changed input for password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Password</legend>
               <input
-                type="text"
+                type="password"
                 className="input"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
